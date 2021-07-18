a9f059a583a831e6c9d68ab9fde68550989a6dbf
zincoder <forebjina@daum.net>
2021-07-18 14:16:26 +0900

    feat(DAY01): CLASS04 의존성 주입 | 4.2 생성자 인젝션 이용

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -1,6 +1,9 @@
 package polymorphism;
 
 public class SamsungTV implements TV{
+	private Speaker speaker;
+	private int price;
+	
 	public void initMethod() {
 		System.out.println("객체 초기화 작업 처리...");
 	}
@@ -10,16 +13,27 @@ public class SamsungTV implements TV{
 	public SamsungTV() {
 		System.out.println("===> SamsungTV 객체 생성");
 	}
+	public SamsungTV(Speaker speaker) {
+		System.out.println("===> SamsungTV(2) 객체 생성");
+		this.speaker = speaker;
+	}
+	public SamsungTV(Speaker speaker, int price) {
+		System.out.println("===> SamsungTV(3) 객체 생성");
+		this.speaker = speaker;
+		this.price = price;
+	}
 	public void powerOn() {
-		System.out.println("SamsungTV---전원 켠다.");
+		System.out.println("SamsungTV---전원 켠다. (가격 : " +price+")");
 	}
 	public void powerOff() {
 		System.out.println("SamsungTV---전원 끈다.");
 	}
 	public void volumeUp() {
-		System.out.println("SamsungTV---소리 올린다.");
+		//speaker = new SonySpeaker();
+		speaker.volumeUp();
 	}
 	public void volumeDown() {
-		System.out.println("SamsungTV---소리 내린다.");
+		//speaker = new SonySpeaker();
+		speaker.volumeDown();
 	}
 }
--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -10,13 +10,11 @@ public class TVUser {
 		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
 		
 		// 2. Spring 컨테이너로부터 필요한 객체를 요청(Lookup)한다.
-		TV tv1 = (TV)factory.getBean("tv");
-		TV tv2 = (TV)factory.getBean("tv");
-		TV tv3 = (TV)factory.getBean("tv");
-//		tv.powerOn();
-//		tv.volumeUp();
-//		tv.powerOff();
-//		tv.volumeDown();
+		TV tv = (TV)factory.getBean("tv");
+		tv.powerOn();
+		tv.volumeUp();
+		tv.powerOff();
+		tv.volumeDown();
 		
 		// 3. Spring 컨테이너를 종료
 		factory.close();
