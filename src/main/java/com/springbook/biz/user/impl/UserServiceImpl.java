636c231d334680ba182ee218bb4082d8e036835e
zincoder <forebjina@daum.net>
2021-07-25 13:31:10 +0900

    feat(DAY01): CLASS07 비즈니스 컴포넌트 실습 2 | 7.1 ~ 7.5  UserService 컴포넌트 생성 _어노테이션 사용하지 않고 Setter인젝션으로 의존성 주입처리

---
+++ src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.springbook.biz.user.impl;
+
+import com.springbook.biz.user.UserService;
+import com.springbook.biz.user.UserVO;
+
+public class UserServiceImpl implements UserService {
+	private UserDAO userDAO;
+	
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+	
+	@Override
+	public UserVO getUser(UserVO vo) {
+		// TODO Auto-generated method stub
+		return userDAO.getUser(vo);
+	}
+
+}
