974d3639a73c65c29f6005755477510eba6431a6
zincoder <forebjina@daum.net>
2021-07-31 19:16:42 +0900

    feat(DAY02): CLASS04 JoinPoint와 바인드 변수

--- src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,13 +1,21 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class AroundAdvice {	
 	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable{
-		System.out.println("[BEFORE]: 비즈니스 메소드 수행 전에 처리할 내용...");
-		Object returnObj = pjp.proceed();
-		System.out.println("[AFTER]: 비즈니스 메소드 수행 후에 처리할 내용...");
-		return returnObj;
+		String method = pjp.getSignature().getName();
+		
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
+		Object obj = pjp.proceed();
+
+		stopWatch.stop();
+		
+		System.out.println(method + "() 메소드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
+		return obj;
 	}
 
 }
