6adf9f841a61e14eead7930f34c3a2ce56758189
zincoder <forebjina@daum.net>
2021-10-03 17:59:36 +0900

    feat(DAY04): CLASS01 어노테이션 기반 mvc 개발

--- src/main/java/com/springbook/view/board/InsertBoardController.java
+++ src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -1,42 +1,35 @@
 package com.springbook.view.board;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
+@Controller
+public class InsertBoardController {
 
-public class InsertBoardController implements Controller {
-
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	@RequestMapping(value="/insertBoard.do")
+	public void insertBoard(BoardVO vo) {
 		System.out.println("글 등록 처리");
 		
 		//1.사용자 입력 정보 추출
 		//request.setCharacterEncoding("EUC-KR");
-		String title = request.getParameter("title");
-		String writer = request.getParameter("writer");
-		String content = request.getParameter("content");
-		
+//		String title = request.getParameter("title");
+//		String writer = request.getParameter("writer");
+//		String content = request.getParameter("content");
+//		
 		//2.DB연동 처리
-		BoardVO vo = new BoardVO();
-		vo.setTitle(title);
-		vo.setWriter(writer);
-		vo.setContent(content);
+//		BoardVO vo = new BoardVO();
+//		vo.setTitle(title);
+//		vo.setWriter(writer);
+//		vo.setContent(content);
 		
 		BoardDAO boardDAO = new BoardDAO();
 		boardDAO.insertBoard(vo);
 		
-		//3.화면 네비게이션
-		//response.sendRedirect("getBoardList.do");
-		//return "getBoardList.do";
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("redirect:getBoardList.do");
-		return mav;
 	}
 
 }
